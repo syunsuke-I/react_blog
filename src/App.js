ba66360e3334068d68f6439cd01ac0fbd66b4e94
syunsuke-I <odol@ymail.ne.jp>
2023-11-18 09:20:30 +0900

    ログインとかも

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom";
 import Home from './components/Home';
+import CreatePost from './components/CreatePost';
+import Login from './components/Login';
+import Logout from './components/Logout';
 
 function App() {
   return(
     <Router>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
+        <Route path="/createPost" element={<CreatePost/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/logout" element={<Logout/>}></Route>
       </Routes>
     </Router>
   );
