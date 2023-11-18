a9ce35397e7af4577e85b10d462e86d1e497655a
syunsuke-I <odol@ymail.ne.jp>
2023-11-18 11:45:21 +0900

    useStateで入力内容を保持

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,8 +1,14 @@
-import React from "react"
+import React, { useState } from "react"
 
 const CreatePost = () => {
+const [title,setTitle] = useState();
+const [postText,setPostText] = useState();
 
-  
+const createPost = ()=>{
+  console.log(title);
+  console.log(postText);
+}
+
 return(
 <div className="min-h-screen bg-gray-100 flex items-center justify-center">
   <div className="p-8 max-w-lg w-full bg-white rounded-lg shadow-md">
@@ -14,6 +20,7 @@ return(
         type="text" 
         placeholder="タイトルを記入" 
         className="w-full p-2 border border-gray-300 rounded mt-1"
+        onChange={(e)=> setTitle(e.target.value)}
       />
     </div>
 
@@ -22,10 +29,15 @@ return(
       <textarea 
         placeholder="内容を記入" 
         className="w-full p-2 border border-gray-300 rounded mt-1 h-40"
-      ></textarea>
+        onChange={(e)=> setPostText(e.target.value)}
+        >
+        </textarea>
     </div>
 
-    <button className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+    <button 
+      className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+      onClick={createPost}
+    >
       投稿する
     </button>
   </div>
